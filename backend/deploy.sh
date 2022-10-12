147b030fe57a6f091c8a05775425751755b376d2
Валерия Шелгунова <valerie.shelgunova@yandex.ru>
2022-10-12 13:52:19 +0000

    Revert "Add new file"
    This reverts commit 58987feeadf948ae90bca465ebe8494f34afb72b

--- backend/deploy.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-set -xe
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/sausage-store-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
