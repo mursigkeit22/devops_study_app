4a9e9ea7d19da5a732fce03a908fcca2cf79d9b8
Валерия Шелгунова <valerie.shelgunova@yandex.ru>
2022-10-13 18:53:03 +0000

    Update deploy-frontend.sh

--- frontend/deploy-frontend.sh
+++ frontend/deploy-frontend.sh
@@ -1,4 +1,7 @@
 set -xe
 sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
+sudo rm -rf /var/www-data/dist/frontend/||true
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-frontend.tar.gz ${NEXUS_REPO_URL}/sausage-store-Valerie-Shelgunova-frontend/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
-sudo tar -xf sausage-store-frontend.tar.gz
+sudo tar -xf sausage-store-frontend.tar.gz /var/www-data/dist
+sudo systemctl daemon-reload
+sudo systemctl restart sausage-store-frontend.service
