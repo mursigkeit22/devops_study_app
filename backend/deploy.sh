7e29ea8a4e773878d7b3f03d89589d0019498ef8
Валерия Шелгунова <valerie.shelgunova@yandex.ru>
2022-10-13 14:48:02 +0000

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,7 +2,7 @@
 set -xe
 sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
 sudo rm -f /home/jarservice/sausage-store.jar||true
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/sausage-store-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/sausage-store-Valerie-Shelgunova-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true 
 sudo systemctl daemon-reload
 sudo systemctl restart sausage-store-backend.service
