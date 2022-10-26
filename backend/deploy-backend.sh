146d756604687cf685584a5ec3ec416870eedb6d
Валерия Шелгунова <valerie.shelgunova@yandex.ru>
2022-10-26 06:58:22 +0000

    Update deploy-backend.sh

--- backend/deploy-backend.sh
+++ backend/deploy-backend.sh
@@ -5,4 +5,4 @@ sudo rm -f /home/jarservice/sausage-store-backend.jar||true
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-backend.jar ${NEXUS_REPO_URL}/sausage-store-Valerie-Shelgunova-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp ./sausage-store-backend.jar /home/jarservice/sausage-store-backend.jar||true 
 sudo systemctl daemon-reload
-sudo systemctl restart sausage-store-backend.service
+sudo systemctl --user restart sausage-store-backend.service
