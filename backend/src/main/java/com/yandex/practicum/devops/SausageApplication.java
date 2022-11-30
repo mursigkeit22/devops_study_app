ad364033342c9127400fce1e8eaa0bf7e813784c
Валерия Шелгунова <valerie.shelgunova@yandex.ru>
2022-11-30 14:39:32 +0000

    Update SausageApplication.java

--- backend/src/main/java/com/yandex/practicum/devops/SausageApplication.java
+++ backend/src/main/java/com/yandex/practicum/devops/SausageApplication.java
@@ -17,7 +17,7 @@ public class SausageApplication {
     @Bean
     CommandLineRunner runner(ProductService productService) {
         return args -> {
-            productService.save(new Product(1L, "Сливочная", 320.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg"));
+            productService.save(new Product(1L, "Сливоч", 320.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg"));
             productService.save(new Product(2L, "Особая", 179.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg"));
             productService.save(new Product(3L, "Молочная", 225.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg"));
             productService.save(new Product(4L, "Нюренбергская", 315.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg"));
