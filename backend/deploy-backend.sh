fea957a366bd05636f7789363665abdbfdc273be
Валерия Шелгунова <valerie.shelgunova@yandex.ru>
2022-12-14 13:29:29 +0000

    Update deploy-backend.sh

--- backend/deploy-backend.sh
+++ backend/deploy-backend.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 set -xe
-sudo cat << EOF > /home/serviceuser/sausagestore.env
+cat << EOF > /home/serviceuser/sausagestore.env
 PSQL_USER=${PSQL_USER}
 PSQL_PASSWORD=${PSQL_PASSWORD}
 PSQL_HOST=${PSQL_HOST}
 PSQL_PORT=${PSQL_PORT}
 PSQL_DBNAME=${PSQL_DBNAME}
 EOF
-sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
+cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
 sudo rm -f /home/jarservice/sausage-store-backend.jar||true
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-backend.jar ${NEXUS_REPO_URL}/sausage-store-Valerie-Shelgunova-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp ./sausage-store-backend.jar /home/jarservice/sausage-store-backend.jar
-sudo systemctl daemon-reload
+systemctl daemon-reload
 sudo systemctl restart sausage-store-backend.service
