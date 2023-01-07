a8b86bafe9729e73c09f93c05d8e3be0a61882be
Валерия Шелгунова <valerie.shelgunova@yandex.ru>
2023-01-07 13:25:37 +0000

    Update deploy-frontend.sh

--- frontend/deploy-frontend.sh
+++ frontend/deploy-frontend.sh
@@ -2,14 +2,10 @@
 set -xe
 sudo usermod -aG docker $USER
 sudo setcap cap_net_bind_service=+ep /usr/bin/node
-docker network create -d bridge sausage_network || true
+sudo usermod -aG docker $USER
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
-docker pull ${CI_REGISTRY_IMAGE}/sausage-frontend:latest
-docker stop sausage-frontend || true
-docker rm sausage-frontend || true
-docker run -d --name sausage-frontend \
-    --network=sausage_network \
-    --restart always \
-    --pull always \
-    -p 80:80 \
-    ${CI_REGISTRY_IMAGE}/sausage-frontend:latest 
+docker-compose stop frontend
+docker-compose rm -f frontend
+docker-compose pull frontend
+docker-compose up -d frontend
+
