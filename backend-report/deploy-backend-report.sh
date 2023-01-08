018422bc79b74df299e265a5f04d14248cf91135
Валерия Шелгунова <valerie.shelgunova@yandex.ru>
2023-01-08 06:01:01 +0000

    Update deploy-backend-report.sh

--- backend-report/deploy-backend-report.sh
+++ backend-report/deploy-backend-report.sh
@@ -7,7 +7,5 @@ PORT=8080
 EOF
 sudo usermod -aG docker $USER
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
-docker-compose stop backend-report
-docker-compose rm -f backend-report
-docker-compose pull backend-report
+
 docker-compose up -d backend-report
